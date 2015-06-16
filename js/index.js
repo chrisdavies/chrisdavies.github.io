fe3424c6dd89a989bbfb2062c00b71f6d7ec81f8
Chris Davies <chris.davies@theironyard.com>
2015-06-16 06:58:19 -0400

    Minor refinements

--- js/index.js
+++ js/index.js
@@ -1,43 +1,28 @@
 (function () {
-    function $one(selector) {
-        return document.querySelector(selector);
+  var pics = [
+    {
+      id: 'cookies',
+      text: 'I like to eat cookies.' 
+    }, {
+      id: 'doodle',
+      text: 'I like doodling in meetings, because everyone thinks I\'m busy taking notes.'
+    }, {
+      id: 'nap',
+      text: 'I like napping.'
+    }, {
+      id: 'mooostache',
+      text: 'I like being ridiculous.'
     }
-
-    function $all(selector, fn) {
-        var nodes = document.querySelectorAll(selector);
-        if (fn) {
-            for (var i = 0; i < nodes.length; ++i) {
-                fn(nodes[i]);
-            }
-        }
-        return nodes;
-    }
-    
-    var silly = [
-        {
-            id: 'cookies',
-            text: 'I like to eat cookies.' 
-        },
-        {
-            id: 'doodle',
-            text: 'I like doodling in meetings, because everyone thinks I\'m busy taking notes.'
-        },
-        {
-            id: 'nap',
-            text: 'I like napping.'
-        },
-        {
-            id: 'mooostache',
-            text: 'I like being ridiculous.'
-        },
-    ];
-
-    var index = Math.floor(Math.random() * silly.length);
-
-    $one('#silly-img').setAttribute('src', '/img/' + silly[index].id + '.png');
-    $one('#silly-text').textContent = silly[index].text;
-    $all('.loading', function (n) {
-        n.classList.remove('loading');
-    });
-    $one('#email-me').setAttribute('href', 'mailto:' + 'ferociousturtle' + '@' + 'gmail.com');
+  ];
+  
+  var index = Math.floor(Math.random() * pics.length);
+  var img = new Image();
+  
+  img.onload = function () {
+    var pic = document.querySelector('.profile-pic');
+    pic.src = img.src;
+    pic.className += ' loaded';
+  };
+  
+  img.src = './img/' + pics[index].id + '.png';
 })();
