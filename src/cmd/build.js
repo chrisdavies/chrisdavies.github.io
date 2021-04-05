473d2b07b06eda345cf419e61f036d071533eb6d
Christopher Davies <dev@christophilus.com>
2021-04-05 16:43:22 -0400

    Update deps, use esbuild for serving. Rebuild dist.

--- src/cmd/build.js
+++ src/cmd/build.js
@@ -1,16 +1,22 @@
 // Compile the ./md folder into ./dist with HTML artifacts n' such
 
-const fs = require('fs');
-const path = require('path');
-const marked = require('marked');
-const hijs = require('highlight.js');
-const childProcess = require('child_process');
-const { promisify } = require('util');
+const fs = require("fs");
+const path = require("path");
+const marked = require("marked");
+const hijs = require("highlight.js");
+const childProcess = require("child_process");
+const { promisify } = require("util");
 const execShell = promisify(childProcess.exec);
 
-
 // The blog article page template
-const articlePage = ({title, content, timestamp, prev, next, script}) => `<!DOCTYPE html>
+const articlePage = ({
+  title,
+  content,
+  timestamp,
+  prev,
+  next,
+  script,
+}) => `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -27,26 +33,29 @@ const articlePage = ({title, content, timestamp, prev, next, script}) => `<!DOCT
       ${content}
     </article>
     <footer class="article-footer">
-      ${!prev ? '' :
-        `<a class="prev-article" href="${prev.filename}">
+      ${
+        !prev
+          ? ""
+          : `<a class="prev-article" href="${prev.filename}">
           ← ${prev.title}
         </a>`
       }
-      ${!next ? '' :
-        `<a class="next-article" href="${next.filename}">
+      ${
+        !next
+          ? ""
+          : `<a class="next-article" href="${next.filename}">
           ${next.title} →
         </a>`
       }
     </footer>
   </main>
-  ${script || ''}
+  ${script || ""}
 </body>
 </html>
 `;
 
-
 // The /blog/index.html template
-const articlesPage = ({articles}) => `<!DOCTYPE html>
+const articlesPage = ({ articles }) => `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -58,17 +67,19 @@ const articlesPage = ({articles}) => `<!DOCTYPE html>
 <body>
   <main class="main article-list">
     <h1>Christopher Davies | Blog</h1>
-    ${articles.map(({title, timestamp, filename}) =>
-      `<a class="article-list-link" href="${filename}">
+    ${articles
+      .map(
+        ({ title, timestamp, filename }) =>
+          `<a class="article-list-link" href="${filename}">
         <span class="article-list-title">${title}</span>
         <time class="article-list-timestamp">${timestamp}</time>
       </a>`
-    ).join('\n')}
+      )
+      .join("\n")}
   </main>
 </body>
 </html>
 `;
-
 
 // The root /index.html template, just redirects to the latest article
 const redirectScript = (url) => `<script>
@@ -82,19 +93,17 @@ const redirectScript = (url) => `<script>
 }());
 </script>`;
 
-
 function readMdFile(mdFile) {
-  return fs.readFileSync(path.join('./src/md', mdFile)).toString();
+  return fs.readFileSync(path.join("./src/md", mdFile)).toString();
 }
 
-
 function loadArticleListItem(mdFile) {
-  const [yyyy, mm, dd] = mdFile.split('-');
+  const [yyyy, mm, dd] = mdFile.split("-");
   const mdContent = readMdFile(mdFile);
-  const titleEnd = mdContent.indexOf('\n');
-  const title = mdContent.slice(0, titleEnd).replace('#', '').trim();
+  const titleEnd = mdContent.indexOf("\n");
+  const title = mdContent.slice(0, titleEnd).replace("#", "").trim();
   const timestamp = `${yyyy}-${mm}-${dd}`;
-  const filename = `${path.basename(mdFile, '.md')}.html`;
+  const filename = `${path.basename(mdFile, ".md")}.html`;
 
   return { title, timestamp, filename, mdFile };
 }
@@ -102,10 +111,10 @@ function loadArticleListItem(mdFile) {
 // Convert a markdown file to HTML
 function writeArticle(article, prev, next, script) {
   const mdContent = readMdFile(article.mdFile);
-  const titleEnd = mdContent.indexOf('\n');
+  const titleEnd = mdContent.indexOf("\n");
   const content = marked(mdContent.slice(titleEnd), {
-    highlight(code, lang) {
-      return hijs.highlight(lang, code).value;
+    highlight(code, language) {
+      return hijs.highlight(code, { language }).value;
     },
   });
   return articlePage({
@@ -117,38 +126,43 @@ function writeArticle(article, prev, next, script) {
   });
 }
 
-
 // Convert ./md/*.md files to blog articles
 async function build() {
   // Clear out the build directory
-  await execShell('rm -rf ./dist');
+  await execShell("rm -rf ./dist");
 
   // Ensure the blog directory exists.
-  fs.mkdirSync('./dist/blog', { recursive: true });
+  fs.mkdirSync("./dist/blog", { recursive: true });
 
   // Convert each markdown file to HTML, and extract the article
   // info for use in building an index page.
-  const articles = fs.readdirSync('./src/md')
+  const articles = fs
+    .readdirSync("./src/md")
     .map(loadArticleListItem)
     .sort((a, b) => b.filename.localeCompare(a.filename));
 
   articles.forEach((article, i) =>
     fs.writeFileSync(
       `./dist/blog/${article.filename}`,
-      writeArticle(article, articles[i-1], articles[i+1]),
-    ),
+      writeArticle(article, articles[i - 1], articles[i + 1])
+    )
   );
-  fs.writeFileSync(`./dist/blog/index.html`, articlesPage({articles}));
-  fs.writeFileSync(`./dist/index.html`,
-    writeArticle(articles[0], undefined, articles[1], redirectScript(`blog/${articles[0].filename}`)),
+  fs.writeFileSync(`./dist/blog/index.html`, articlesPage({ articles }));
+  fs.writeFileSync(
+    `./dist/index.html`,
+    writeArticle(
+      articles[0],
+      undefined,
+      articles[1],
+      redirectScript(`blog/${articles[0].filename}`)
+    )
   );
 
   // TODO: Prolly should optimize CSS via postcss or something...
-  fs.copyFileSync('./src/css/main.css', './dist/blog/main.css');
+  fs.copyFileSync("./src/css/main.css", "./dist/blog/main.css");
 
-  fs.copyFileSync('./src/img/favicon.ico', './dist/favicon.ico');
+  fs.copyFileSync("./src/img/favicon.ico", "./dist/favicon.ico");
 }
-
 
 // Do the thing.
 build();
