79db2fcd6779da69f44d3b8d81ba938b9b634c72
Christopher Davies <dev@christophilus.com>
2021-12-23 16:36:58 -0500

    The evolution of style. (The blog post, and also the CSS changes)

--- src/cmd/build.js
+++ src/cmd/build.js
@@ -2,7 +2,7 @@
 
 const fs = require("fs");
 const path = require("path");
-const marked = require("marked");
+const { marked } = require("marked");
 const hijs = require("highlight.js");
 const childProcess = require("child_process");
 const { promisify } = require("util");
